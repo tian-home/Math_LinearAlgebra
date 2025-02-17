4e5db081fbb1bd941d1c3cac7bc169015dc110b9
tian-home <tian-home@outlook.com>
2025-02-17 19:01:41 +0800

    1st

---
+++ main_vector.py
@@ -0,0 +1,29 @@
+from playLA.Vector import Vector
+
+if __name__ == "__main__":
+
+    vec = Vector([5, 2])
+    print(vec)
+    print(len(vec))
+    print(vec[0], vec[1])
+
+    vec2 = Vector([3,1])
+
+    print(vec - vec2)
+    print(vec * 3)
+    print(4 * vec)
+    print(-vec)
+    print(+vec)
+    zero2 = Vector.zero(2)
+    print(zero2)
+    print(vec.norm())
+    print(vec.normalioze())
+    print(vec.normalioze().norm())
+    print(vec2.normalioze())
+    print(vec2.normalioze().norm())
+
+
+    try:
+        zero2.normalioze()
+    except ZeroDivisionError:
+        print("can not")
---
+++ playLA/Vector.py
@@ -0,0 +1,58 @@
+from ._global import EPSILON
+import math
+
+class Vector:
+    def __init__(self, lst):
+        self._values = list(lst)
+
+    @classmethod
+    def zero(cls, dim):
+        return cls([0] * dim)
+
+    def norm(self):
+        return math.sqrt(sum(e**2 for e in self))
+
+    def normalioze(self):
+        # return Vector([e/self.norm() for e in self])
+        # return 1/self.norm() * Vector(self._values)
+        if self.norm() < EPSILON:
+            raise ZeroDivisionError("Normalize error! norm is zero")
+        return Vector(self._values) / self.norm()
+
+    def __add__(self, another):
+        assert len(self) == len(another), "Error"
+        return Vector([a + b for a, b in zip(self, another)])
+
+    def __sub__(self, another):
+        assert len(self) == len(another), "Error"
+        return Vector([a - b for a, b in zip(self, another)])
+
+    def __mul__(self, k):
+        return Vector([k*e for e in self])
+
+    def __rmul__(self, k):
+        return Vector([k*e for e in self])
+
+    def __truediv__(self, k):
+        return (1/k) * self
+
+    def __pos__(self):
+        return 1 * self
+
+    def __neg__(self):
+        return -1 * self
+
+    def __iter__(self):
+        return self._values.__iter__()
+
+    def __getitem__(self, index):
+        return self._values[index]
+
+    def __len__(self):
+        return len(self._values)
+
+    def __repr__(self):
+        return "Vector({})".format(self._values)
+
+    def __str__(self):
+        return "({})".format(", ".join(str(e) for e in self._values))
